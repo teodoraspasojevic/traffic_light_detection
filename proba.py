a239e0dc7f6dae1817d9a08ba833d08a042d565a
teodoraspasojevic <spasojevic.teodora@gmail.com>
2023-06-20 16:05:02 +0200

    Create proba.py

---
+++ proba.py
@@ -0,0 +1,20 @@
+import torch
+
+# Model
+model = torch.hub.load("ultralytics/yolov5", "yolov5m")  # or yolov5n - yolov5x6, custom
+
+# Images
+img = "https://ultralytics.com/images/zidane.jpg"  # or file, Path, PIL, OpenCV, numpy, list
+
+# Inference
+results = model(img)
+
+# Results
+print('results beg')
+results.show()  # or .show(), .save(), .crop(), .pandas(), etc.
+print('results end')
+
+# Model parameters
+print('Model parameters:')
+for k, v in model.named_parameters():
+    print(k)
