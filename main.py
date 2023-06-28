355ac988eac5280f95ff8a5eebac1bc4f1a74695
teodoraspasojevic <spasojevic.teodora@gmail.com>
2023-06-28 18:32:08 +0200

    training_rw1

--- main.py
+++ main.py
@@ -4,17 +4,30 @@ from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 from models.yolo import Model
 
+TRAIN_PATH = '/home/rtrk/teodora/datasets/traffic_light_real_world/train'
+TEST_PATH = '/home/rtrk/teodora/datasets/traffic_light_real_world/test'
+
 # Load the pre-trained YOLOv5 model
-model = Model('yolov5m.yaml')
+model = Model('/home/rtrk/teodora/traffic_light_detection/models/yolov5m.yaml')
 
 # Freeze all layers except the last one
 for param in model.parameters():
     param.requires_grad = False
-model.model[-1].conv[-1].weight.requires_grad = True
-model.model[-1].conv[-1].bias.requires_grad = True
+# print(model.model)
+print(type(model))                   # model - model.yolo.DetectionModel class - backbone (model.yolo.BaseModel) + head (model.yolo.Detect)
+print(type(model.model))             # model.model - torch.nn.modules.container.Sequential - contains all of the layers
+print(type(model.model[-1]))         # model.model[-1] - last layer - model.yolo.Detect - has ModuleList with three Conv2d layers
+print(model.model[-1])
+print(type(model.model[-1].m))       # model.model[-1].m - torch.nn.modules.container.ModuleList
+print(model.model[-1].m)
+print(type(model.model[-1].m[-1]))   # model.model[-1].m[-1] - last Conv2d in last layer
+print(model.model[-1].m[-1].weight.requires_grad)
+
+model.model[-1].m[-1].weight.requires_grad = True
+model.model[-1].m[-1].bias.requires_grad = True
 
 # Load the custom dataset
-train_dataset = datasets.ImageFolder('path/to/train/dataset', transform=transforms.ToTensor())
+train_dataset = datasets.ImageFolder(TRAIN_PATH, transform=transforms.ToTensor())
 train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
 
 # Train the last layer of the model
@@ -29,7 +42,7 @@ for epoch in range(10):
         optimizer.step()
 
 # Evaluate the model
-test_dataset = datasets.ImageFolder('path/to/test/dataset', transform=transforms.ToTensor())
+test_dataset = datasets.ImageFolder(TEST_PATH, transform=transforms.ToTensor())
 test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
 correct = 0
 total = 0
